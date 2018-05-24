a8577dcbfece4e07d29ac38a05bd32548587cbf4
Michelle S Lee <michlee1337@gmail.com>
2018-05-24 15:13:16 +0800

    completed sigmoid function

--- ex2/costFunction.m
+++ ex2/costFunction.m
@@ -7,7 +7,7 @@ function [J, grad] = costFunction(theta, X, y)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
 
@@ -21,9 +21,9 @@ grad = zeros(size(theta));
 %
 
 
-J = (1/m)*(-y*sigmoid(X*theta)-((1-y)*log(1-sigmoid(X*theta)));
+%J = (1/m)*(-y*sigmoid(X*theta)-((1-y)*log(1-sigmoid(X*theta)));
 
-grad = (1/m)*(((theta*x)-y)*x)
+%grad = (1/m)*(((theta*x)-y)*x)
 
 
 % =============================================================
